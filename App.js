394f5a1a1af624c59b8c1b837cef86e87fafede2
b-mythili <mythilib36@gmail.com>
2025-02-03 13:32:19 +0530

    day2 react

---
+++ App.js
@@ -0,0 +1,25 @@
+
+ import React from "react";
+// function App() {
+//   return (
+//     <section>
+      
+//       <h1 style={{color:'red'}}>one hr </h1>
+//        <p>{100+100}</p>
+      
+//     </section>
+//   );
+// }
+
+// export default App;
+class App extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>one hr </h1>
+        <p>{100 + 100}</p>
+      </section>
+    );
+  }
+}
+export default App;
